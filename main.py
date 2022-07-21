a61a40741a0bc80555f7bd60d749116a875de798
aqulline <aqeglipambuya@gmail.com>
2022-07-21 10:58:03 +0300

    login and home running soomtth
    damn today got some free internet shit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import re
 
-from kivy.properties import StringProperty, ObjectProperty
+from kivy.properties import StringProperty, ObjectProperty, NumericProperty
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy import utils
-from kivy.clock import Clock
+from kivy.base import EventLoop
 from kivymd.toast import toast
 
 from kivymd.uix.textfield import MDTextField
@@ -33,8 +33,13 @@ class NumberOnlyField(MDTextField):
 
 
 class MainApp(MDApp):
-    size_x, size_y = 0, 0
+    size_x, size_y = NumericProperty(0), NumericProperty(0)
     sm = ObjectProperty()
+
+    # screen
+    screens = ["genesis"]
+    screens_size = NumericProperty(len(screens) - 1)
+    current = StringProperty(screens[len(screens) - 1])
 
     # DUMMY VARS
     dummy_cash = StringProperty("0")
@@ -46,6 +51,12 @@ class MainApp(MDApp):
 
     def on_start(self):
         self.sm = self.root
+        self.keyboard_hooker()
+        self.backgrounds()
+
+    def backgrounds(self):
+        plus_button = self.root.ids.plus
+        plus_button.md_bg_color = 36 / 255, 146 / 255, 255 / 255, 1
 
     """
     
@@ -72,7 +83,7 @@ class MainApp(MDApp):
         if self.code_bool:
             code = self.root.ids.code
             self.passcode = code.text
-            self.sm.current = "account"
+            self.sm.current = "genesis"
         if not self.code_bool:
             toast("Code not Match!")
 
@@ -97,6 +108,53 @@ class MainApp(MDApp):
         """
 
     """
+            Down Here Stays Screen Function
+    
+    """
+
+    def keyboard_hooker(self):
+        EventLoop.window.bind(on_keyboard=self.hook_keyboard)
+
+    def hook_keyboard(self, window, key, *largs):
+        print(self.screens_size)
+        if key == 27 and self.screens_size > 0:
+            print(f"your were in {self.current}")
+            last_screens = self.current
+            self.screens.remove(last_screens)
+            print(self.screens)
+            self.screens_size = len(self.screens) - 1
+            self.current = self.screens[len(self.screens) - 1]
+            self.screen_capture(self.current)
+            return True
+        elif key == 27 and self.screens_size == 0:
+            toast('Press Home button!')
+            return True
+
+    def screen_capture(self, screen):
+        sm = self.root
+        sm.current = screen
+        if screen in self.screens:
+            pass
+        else:
+            self.screens.append(screen)
+        print(self.screens)
+        self.screens_size = len(self.screens) - 1
+        self.current = self.screens[len(self.screens) - 1]
+        print(f'size {self.screens_size}')
+        print(f'current screen {screen}')
+
+    def screen_leave(self):
+        print(f"your were in {self.current}")
+        last_screens = self.current
+        self.screens.remove(last_screens)
+        print(self.screens)
+        self.screens_size = len(self.screens) - 1
+        self.current = self.screens[len(self.screens) - 1]
+        self.screen_capture(self.current)
+
+    """End of Screen Functions"""
+
+    """
     
         TESTING FUNCTIONS DOWN HERE
     
@@ -108,7 +166,7 @@ class MainApp(MDApp):
 
     def build(self):
         self.size_x, self.size_y = Window.size
-        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "DeepPurple"
         self.theme_cls.accent = "Brown"
         self.size_x, self.size_y = Window.size
